1c8d1bef0ae2920a7001e5f5d281913f1ce3aeef
Andrea Vitullo <andrea.vitullo27@gmail.com>
2024-03-20 14:23:33 +0000

    modify sorting scripts

--- formant_sorting.py
+++ formant_sorting.py
@@ -1,18 +1,15 @@
 import pandas as pd
-import shutil
 import os
 
 import my_config
+from utils import file_management
 
-# Load the updated csv file
 train_df = pd.read_csv(my_config.TRAIN_SPLIT)
 dev_df = pd.read_csv(my_config.DEV_SPLIT)
 test_df = pd.read_csv(my_config.TEST_SPLIT)
 
-# specify the current directory where your png files are
 source_dir = my_config.FORMANT_FEATURES
 
-# specify the directory where you want to move the png files
 train_dir_0 = my_config.FORM_TRAIN_DIR_0
 train_dir_1 = my_config.FORM_TRAIN_DIR_1
 
@@ -32,30 +29,17 @@ os.makedirs(test_dir_0, exist_ok=True)
 os.makedirs(test_dir_1, exist_ok=True)
 
 
-def file_sorter(file_path, dir_0, dir_1, binary_value, filename, source_file_path):
-    if os.path.exists(source_file_path):
-
-        # Determine destination directory based on binary_value
-        dest_dir = dir_1 if binary_value == 1 else dir_0
-
-        # Create destination file path
-        dest_file_path = os.path.join(dest_dir, filename)
-
-        # Move the file to the appropriate destination directory
-        shutil.move(file_path, dest_file_path)
-        print(f'Moved file: {filename} to {dest_dir}\n')
-    else:
-        print(f'File not found: {source_file_path}\n')
-
-
 def dataframe_iterator(dataframe, dir_0, dir_1):
     for index, row in dataframe.iterrows():
         participant_id = str(int(row['Participant_ID']))  # Ensure participant_id is string type
-        binary_value = int(row['PHQ8_Binary'])  # PHQ8 for train and dev -- PHQ for test
+        if dataframe is test_df:
+            binary_value = int(row['PHQ_Binary'])
+        else:
+            binary_value = int(row['PHQ8_Binary'])  # PHQ8 for train and dev -- PHQ for test
         filename = f"{participant_id}_features.csv" or f"{participant_id}_augmented_features.csv"
         source_file_path = os.path.join(source_dir, filename)
 
-        file_sorter(source_file_path, dir_0, dir_1, binary_value, filename, source_file_path)
+        file_management.file_sorter(source_file_path, dir_0, dir_1, binary_value, filename, source_file_path)
 
 
 dataframe_iterator(train_df, train_dir_0, train_dir_1)
--- utils/file_management.py
+++ utils/file_management.py
@@ -15,6 +15,7 @@ def wave_mover():
 
     Args:
         -
+
     Returns:
         None
     """
@@ -28,7 +29,24 @@ def wave_mover():
 
 
 def save_outputs(features, audio, path):
-    """Save features to a CSV file."""
+    """
+    Save given features to a CSV file.
+
+    This function takes a list of feature arrays, converts it to a DataFrame,
+    and then writes that DataFrame to a CSV file. The output CSV file is saved
+    to the specified path with a name based on the input audio file name.
+
+    It prints a message to inform where the .csv file has been saved.
+
+    Args:
+        features (array-like): A list of audio feature arrays.
+        audio (str): Name of the audio file. The first three characters of this
+            name are used as the base for the output filename.
+        path (str): Directory path where the output CSV file will be saved.
+
+    Returns:
+        None
+    """
 
     df = pd.DataFrame(features)
     # n = 5  # The number of columns to keep (replace as needed)
@@ -37,3 +55,38 @@ def save_outputs(features, audio, path):
     output_filename = os.path.join(path, f'{audio[0:3]}_features.csv')
     df.to_csv(output_filename, index=False)
     print(f"Saving .csv files: {output_filename}\n")
+
+
+def file_sorter(file_path, dir_0, dir_1, binary_value, filename, source_file_path):
+    """
+    Sorts and moves a file to a specific directory based on a binary value.
+
+    This function checks if the source file exists, determines the appropriate destination
+    directory based on the binary value, then moves the file from source to the dedicated
+    destination directory. It prints a successful move operation or a file not found error.
+
+    Args:
+        file_path (str): The path of the file to be moved.
+        dir_0 (str): The destination directory for files with a binary value of 0.
+        dir_1 (str): The destination directory for files with a binary value of 1.
+        binary_value (int): Binary flag (0 or 1) to determine the destination directory for file.
+        filename (str): The name of the file to be moved.
+        source_file_path (str): Path of the file in the source directory.
+
+    Returns:
+        None
+    """
+
+    if os.path.exists(source_file_path):
+
+        # Determine destination directory based on binary_value
+        dest_dir = dir_1 if binary_value == 1 else dir_0
+
+        # Create destination file path
+        dest_file_path = os.path.join(dest_dir, filename)
+
+        # Move the file to the appropriate destination directory
+        shutil.move(file_path, dest_file_path)
+        print(f'Moved file: {filename} to {dest_dir}\n')
+    else:
+        print(f'File not found: {source_file_path}\n')
