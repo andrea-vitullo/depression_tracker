52b82e2e56562ad9c8203cac28f9d19a8321c2f2
Andrea Vitullo <andrea.vitullo27@gmail.com>
2024-04-04 10:30:39 +0100

    modify model_tester

--- 1d_model.py
+++ 1d_model.py
@@ -12,6 +12,7 @@ import my_config
 from my_config import LOGMEL_SHAPE_WINDOW, EPOCHS
 from utils import utils
 from data_loader import DataLoader
+from model_tester import ModelTester
 from attention_layer import Attention, squeeze_excite_block
 
 
@@ -24,29 +25,29 @@ data_loader = DataLoader(my_config.TRAIN_H5, my_config.DEV_H5, my_config.TEST_H5
 audio_input = Input(shape=LOGMEL_SHAPE_WINDOW)
 
 # Conv2D Layer
-conv1 = Conv1D(filters=64, kernel_size=80, strides=1, padding='valid')(audio_input)
+conv1 = Conv1D(filters=256, kernel_size=120, strides=1, padding='valid')(audio_input)
 # conv1 = squeeze_excite_block(conv1)
 # conv1 = Activation('relu')(conv1)
 
 conv1 = Dropout(0.5)(conv1)
 
-conv2 = Conv1D(filters=128, kernel_size=40, strides=1, padding='valid')(conv1)
+# conv2 = Conv1D(filters=128, kernel_size=40, strides=1, padding='valid')(conv1)
 # conv2 = squeeze_excite_block(conv2)
 # # conv2 = Activation('relu')(conv2)
 
-conv2 = Dropout(0.5)(conv2)
+# conv2 = Dropout(0.5)(conv2)
 
 # MaxPooling2D Layer
-max_pool1 = MaxPooling1D(pool_size=1, strides=1, padding='valid')(conv2)
+max_pool1 = MaxPooling1D(pool_size=1, strides=1, padding='valid')(conv1)
 
 reshape = layers.Reshape((-1, 1))(max_pool1)
 
 # Add the GRU layer
-gru = Attention(return_sequences=True)(layers.GRU(32, return_sequences=True)(reshape))
-gru2 = Attention(return_sequences=True)(layers.GRU(32, return_sequences=True)(gru))
-gru3 = Attention(return_sequences=True)(layers.LSTM(32, return_sequences=True)(gru2))
+gru = layers.GRU(64, return_sequences=True)(reshape)
+gru2 = layers.GRU(32, return_sequences=True)(gru)
+gru3 = layers.LSTM(32, return_sequences=True)(gru2)
 # gru4 = layers.LSTM(128, return_sequences=True)(gru3)
-gru5 = Attention(return_sequences=False)(layers.LSTM(32, return_sequences=True)(gru3))
+gru5 = layers.LSTM(32, return_sequences=False)(gru3)
 
 
 output = Dense(1, activation='sigmoid', kernel_regularizer=regularizers.l2(0.03))(gru5)
@@ -109,3 +110,11 @@ def plot_history(hstory):
 
 
 plot_history(history)
+
+
+######################################################################################################################
+
+
+data_loader = DataLoader(my_config.TRAIN_H5, my_config.DEV_H5, my_config.TEST_H5)
+tester = ModelTester('./model/1d_model.keras', data_loader)
+tester.test()
