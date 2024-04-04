17c32e5798238896114ca6a1053d88d520dd8e17
Andrea Vitullo <andrea.vitullo27@gmail.com>
2024-04-04 10:03:55 +0100

    add 1d attention layer

--- 1d_model.py
+++ 1d_model.py
@@ -1,119 +1,52 @@
-import tensorflow as tf
 import tensorflow.keras as keras
 from keras import layers, regularizers
 from keras.layers import Input, LSTM, Dense, Activation, MaxPooling1D, Conv1D, Dropout, BatchNormalization
 from keras.utils import plot_model
 from keras.models import Model
 from keras.callbacks import ReduceLROnPlateau, EarlyStopping, LearningRateScheduler
-from keras import backend as K
 import matplotlib.pyplot as plt
 import logging
 
 
 import my_config
-from my_config import LOGMEL_SHAPE_WINDOW, EPOCHS, BATCH_SIZE
+from my_config import LOGMEL_SHAPE_WINDOW, EPOCHS
 from utils import utils
-from data_generator import DataGenerator
+from data_loader import DataLoader
 from attention_layer import Attention, squeeze_excite_block
 
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-
-train_generator = DataGenerator(
-    my_config.TRAIN_H5,
-    batch_size=BATCH_SIZE,
-    audio_shape=LOGMEL_SHAPE_WINDOW,
-    verbose=False
-)
-
-dev_generator = DataGenerator(
-    my_config.DEV_H5,
-    batch_size=BATCH_SIZE,
-    audio_shape=LOGMEL_SHAPE_WINDOW,
-    verbose=False
-)
-
-
-def train_gen():
-    for features, labels in train_generator:
-        yield features, labels
-
-
-# Adjust the output_signature according to your actual data shape and types
-train_dataset = tf.data.Dataset.from_generator(
-    train_gen,
-    output_signature=(
-        {
-            "input_1": tf.TensorSpec(shape=(None,) + LOGMEL_SHAPE_WINDOW, dtype=tf.float32),  # Adjust shape
-        },
-        tf.TensorSpec(shape=(None,), dtype=tf.float32),  # Adjust shape for your labels
-    ),
-)
-
-
-for features, labels in train_dataset.take(1):
-    print("Features shape:", features["input_1"].shape)
-    print("Labels shape:", labels.shape)
-
-
-def dev_gen():
-    for features, labels in dev_generator:
-        yield features, labels
-
-
-# Adjust the output_signature according to your actual data shape and types
-dev_dataset = tf.data.Dataset.from_generator(
-    dev_gen,
-    output_signature=(
-        {
-            "input_1": tf.TensorSpec(shape=(None,) + LOGMEL_SHAPE_WINDOW, dtype=tf.float32),  # Adjust shape
-        },
-        tf.TensorSpec(shape=(None,), dtype=tf.float32),  # Adjust shape for your labels
-    ),
-)
+data_loader = DataLoader(my_config.TRAIN_H5, my_config.DEV_H5, my_config.TEST_H5)
 
 
 # Input layer
 audio_input = Input(shape=LOGMEL_SHAPE_WINDOW)
 
 # Conv2D Layer
-conv1 = Conv1D(filters=128, kernel_size=120, strides=1, padding='valid')(audio_input)
+conv1 = Conv1D(filters=64, kernel_size=80, strides=1, padding='valid')(audio_input)
 # conv1 = squeeze_excite_block(conv1)
 # conv1 = Activation('relu')(conv1)
 
-# conv2 = Conv2D(filters=128, kernel_size=(3, 10), strides=(1, 1), padding='valid')(conv1)
+conv1 = Dropout(0.5)(conv1)
+
+conv2 = Conv1D(filters=128, kernel_size=40, strides=1, padding='valid')(conv1)
 # conv2 = squeeze_excite_block(conv2)
 # # conv2 = Activation('relu')(conv2)
-#
-conv1 = Dropout(0.7)(conv1)
+
+conv2 = Dropout(0.5)(conv2)
 
 # MaxPooling2D Layer
-max_pool1 = MaxPooling1D(pool_size=1, strides=1, padding='valid')(conv1)
-
-
-# Preparing for LSTM
-# flattened = layers.Flatten()(max_pool1)
+max_pool1 = MaxPooling1D(pool_size=1, strides=1, padding='valid')(conv2)
 
 reshape = layers.Reshape((-1, 1))(max_pool1)
 
 # Add the GRU layer
-gru = layers.GRU(32, return_sequences=True)(reshape)
-gru2 = layers.GRU(32, return_sequences=True)(gru)
-gru3 = layers.LSTM(32, return_sequences=True)(gru2)
+gru = Attention(return_sequences=True)(layers.GRU(32, return_sequences=True)(reshape))
+gru2 = Attention(return_sequences=True)(layers.GRU(32, return_sequences=True)(gru))
+gru3 = Attention(return_sequences=True)(layers.LSTM(32, return_sequences=True)(gru2))
 # gru4 = layers.LSTM(128, return_sequences=True)(gru3)
-gru5 = layers.LSTM(32, return_sequences=False)(gru3)
-
-# # Assuming max_pool1 has shape (batch_size, height, width, channels)
-# batch_size, height, width, channels = K.int_shape(max_pool1)
-# reshaped = layers.Reshape((height, width * channels))(max_pool1)
-
-
-# # LSTM layers
-# lstm_layer_1 = LSTM(128, return_sequences=True)(reshaped)  # Single LSTM layer
-# lstm_layer_2 = LSTM(128, return_sequences=True)(lstm_layer_1)
-# lstm_layer_3 = LSTM(128, return_sequences=False)(lstm_layer_2)
-
+gru5 = Attention(return_sequences=False)(layers.LSTM(32, return_sequences=True)(gru3))
 
 
 output = Dense(1, activation='sigmoid', kernel_regularizer=regularizers.l2(0.03))(gru5)
@@ -141,7 +74,7 @@ callbacks = [
 
 epochs = EPOCHS
 
-history = model.fit(train_dataset, epochs=epochs, validation_data=dev_dataset, callbacks=callbacks, verbose=1)
+history = model.fit(data_loader.train_dataset, epochs=epochs, validation_data=data_loader.dev_dataset, callbacks=callbacks, verbose=1)
 
 
 ######################################################################################################################
--- attention_layer.py
+++ attention_layer.py
@@ -11,9 +11,6 @@ class Attention(tf.keras.layers.Layer):
     def build(self, input_shape):
         self.W = self.add_weight(name="att_weight", shape=(input_shape[-1], 1),
                                  initializer="normal")
-        self.b = self.add_weight(name="att_bias", shape=(input_shape[1], 1),
-                                 initializer="zeros")
-
         super(Attention, self).build(input_shape)
 
     def call(self, x):
@@ -47,6 +44,27 @@ def squeeze_excite_block(input):
     if K.image_data_format() == 'channels_first':
         se = Permute((3, 1, 2))(se)
 
+    x = multiply([input, se])
+
+    return x
+
+
+def squeeze_excite_block_1d(input):
+    """
+    Create a squeeze-excite block for 1D input
+
+    Args:
+        input: input tensor
+        filters: number of output filters
+
+    Returns: a keras tensor
+    """
+
+    filters = input.shape[-1] # channel_axis = -1 for TF
+    se = tf.keras.layers.GlobalAveragePooling1D()(input)
+    se = Reshape((1, filters))(se)
+    se = Dense(filters//16, activation='relu', kernel_initializer='he_normal', use_bias=False)(se)
+    se = Dense(filters, activation='sigmoid', kernel_initializer='he_normal', use_bias=False)(se)
     x = multiply([input, se])
 
     return x
