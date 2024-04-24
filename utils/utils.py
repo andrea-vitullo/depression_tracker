1d2026683c175319a2c4cb53e782713418073d1b
Andrea Vitullo <andrea.vitullo27@gmail.com>
2024-04-24 09:45:49 +0100

    add function descriptors

--- utils/utils.py
+++ utils/utils.py
@@ -4,51 +4,31 @@ import numpy as np
 import os
 import librosa
 
-from my_config import *
-# from data_generator import DataGenerator
-# from features_extractors import extract_raw_audio, extract_mfcc, extract_logmel
+import my_config
 
 
 def lr_scheduler(epoch, lr):
-    if epoch > 0 and epoch % EPOCHS_DROP == 0:
-        return lr * DECAY_FACTOR
+    if epoch > 0 and epoch % my_config.EPOCHS_DROP == 0:
+        return lr * my_config.DECAY_FACTOR
     return lr
 
 
-# def create_datagenerator(extraction_function, train_filepath, dev_filepath, test_filepath, batch_size):
-#     # Create the appropriate DataGenerator based on extraction_function
-#     if extraction_function == extract_raw_audio:
-#         train_generator = DataGenerator(train_filepath, batch_size, audio_shape=RAW_SHAPE)
-#         dev_generator = DataGenerator(dev_filepath, batch_size, audio_shape=RAW_SHAPE)
-#         test_generator = DataGenerator(test_filepath, batch_size, audio_shape=RAW_SHAPE)
-#     elif extraction_function == extract_mfcc:
-#         train_generator = DataGenerator(train_filepath, batch_size, audio_shape=MFCC_SHAPE)
-#         dev_generator = DataGenerator(dev_filepath, batch_size, audio_shape=MFCC_SHAPE)
-#         test_generator = DataGenerator(test_filepath, batch_size, audio_shape=MFCC_SHAPE)
-#     elif extraction_function == extract_logmel:
-#         train_generator = DataGenerator(train_filepath, batch_size, audio_shape=LOGMEL_SHAPE)
-#         dev_generator = DataGenerator(dev_filepath, batch_size, audio_shape=LOGMEL_SHAPE)
-#         test_generator = DataGenerator(test_filepath, batch_size, audio_shape=LOGMEL_SHAPE)
-#     else:
-#         raise ValueError("Invalid extraction_function. Must be one of [extract_raw_audio, extract_mfcc, extract_logmel]")
-#
-#     return train_generator, dev_generator, test_generator
-
-
-# def load_files_labels(directories, labels):
-#     files = []
-#     label_list = []
-#
-#     for directory, label in zip(directories, labels):
-#         for gender in ['male', 'female']:
-#             gender_files = glob.glob(f"{directory}/{gender}/*.wav")
-#             files.extend(gender_files)
-#             label_list.extend([label] * len(gender_files))
-#
-#     return files, label_list
-
-
 def load_files_labels(base_dir):
+    """
+    Loads audio files and their corresponding labels from a given directory.
+
+    This function scans through subdirectories within a base directory according to
+    predefined patterns, and, for each .wav file, it adds the file path and its corresponding
+    label to the lists. The labels are mapped from the patterns detected in the file path such
+    as 'non_depressed/male', 'non_depressed/female', 'depressed/male', 'depressed/female'.
+
+    Args:
+        base_dir (str): The base directory containing the audio files.
+
+    Returns:
+        files (list): List of audio file paths.
+        labels (list): Corresponding list of labels for the audio files.
+    """
 
     files = []
     labels = []
@@ -79,6 +59,20 @@ def load_files_labels(base_dir):
 
 
 def load_features(file_path):
+    """
+    Loads audio data and labels from an HDF5 file.
+
+    This function opens an HDF5 file and reads each key's 'audio' data and its 'label' attribute.
+    Assumes that all audio samples are of the same shape and that labels
+    are scalar values.
+
+    Args:
+        file_path (str): The filename of the HDF5 file.
+
+    Returns:
+        audio_data (np.ndarray): Numpy array of the audio data.
+        labels (np.ndarray): Corresponding numpy array of labels.
+    """
 
     with h5py.File(file_path, 'r') as h5f:
         # Let's assume we know the number of samples beforehand
